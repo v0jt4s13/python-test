2ddea492ddf0177c86bd46575c908f63384d2852
Wojtek <github@wmarzec.eu>
2022-03-05 04:29:34 +0100

    londynek sitemap generator update

--- test.py
+++ test.py
@@ -1,122 +1,103 @@
-#import json
-#import subprocess
-from lista_flag import stworz_liste_flag
+import subprocess
+import os
 
-def najdluzsza_najkrotsza_domena(lista_flag):
-  n_dlugosc_max = 0 
-  dlugie_domeny = []
-  najdluzsza_domena = [] 
-  n_dlugosc_min = 0 
-  krotkie_domeny = [] 
-  najkrotsza_domena = []
-
-  ### Najdłuższa domena 
-  for flaga in lista_flag:
-    if len(flaga) > n_dlugosc_max: 
-      n_dlugosc_max = len(flaga)
-      dlugie_domeny.append(flaga)
-
-  for flaga in dlugie_domeny:
-    if len(flaga) == n_dlugosc_max:
-      najdluzsza_domena.append(flaga)
-
-  ### Najkrótsza domena
-  n_dlugosc_min = n_dlugosc_max
-
-  for flaga in lista_flag:
-    if len(flaga) <= n_dlugosc_min and len(flaga) > 0:
-      n_dlugosc_min = len(flaga)
-      krotkie_domeny.append(flaga)
-
-  for flaga in krotkie_domeny:
-    if len(flaga) == n_dlugosc_min: 
-      najkrotsza_domena.append(flaga)
-
-  wynik = 'Najdłuższa domena to: ', najdluzsza_domena,', a najkrótsza domena to:', najkrotsza_domena
-
-  return wynik
+def check_code_safety(file_path):
+    code_lines = open(file_path).readlines()
+    code_lines = [l.strip() for l in code_lines]
+    banned_libraries = ['os', 'subprocess']
+    # allowed_libraries = ['random', 'string'] # ta linia jest niepotrzebna
+    file_name = file_path.split('/')[-1]
+    for l in code_lines:
+      l = l.strip(' ')
+      l_list = l.split(' ')
+      for word in l_list:
+            if word in banned_libraries:
+              if "import" in l_list:
+                print('W pliku: '+file_name+' ==> znalezione zostało słowo: '+word+' w linii: '+l)
+                return False
+    return True
 
 
+def check_code_safety_orig(file_path):
+    save = False # True
+
+    code_lines = open(file_path).readlines()
+    code_lines = [l.strip() for l in code_lines]
+
+    for l in code_lines:
+
+        # Downloads.
+        download_words = ['']
+        for word in download_words:
+            if word in l:
+                return False
+
+        # Polecenie remove.
+
+        banned_words = ['']
+        for word in banned_words:
+            if word in l:
+                return False
+        # Sudo
+        banned_words = ['sudo', 'su']
+        for word in banned_words:
+            if word in l:
+                return False
+
+        # Polecenia os, popen, sys = [ ]
+
+        # Lista zbanowanych bibliotek.
+        banned_libraries = ['os', 'subprocess', '']
+        allowed_libraries = ['random', 'string']
+        for word in banned_words:
+            if word in l:
+                return False
 
 
-def najdluzsza_najkrotsza_domena1 (lista_flag):
-  n_dlugosc_max = 0 
-  dlugie_domeny = []
-  najdluzsza_domena = [] 
-  n_dlugosc_min = 0 
-  krotkie_domeny = [] 
-  najkrotsza_domena = []
+    return True
 
-  ### Najdłuższa domena 
-  for flaga in lista_flag:
-    flaga_len = len(flaga)
-    if flaga_len > 0:
-      if flaga_len > n_dlugosc_max: 
-        n_dlugosc_max = flaga_len
-        dlugie_domeny.append(flaga)
-      elif flaga_len <= n_dlugosc_min or n_dlugosc_min == 0:
-        n_dlugosc_min = flaga_len
-        krotkie_domeny.append(flaga)
-  
-  print('\nIlość znaków (%i) najdłuższej domeny: %s \nIlość znaków (%i) najkrótszej domeny: %s \n\n' %(len(dlugie_domeny[-1]),dlugie_domeny[-1], len(krotkie_domeny[-1]),krotkie_domeny[-1]))
-  
-  for flaga in dlugie_domeny:
-    if len(flaga) == n_dlugosc_max:
-      najdluzsza_domena.append(flaga)
+lista_plikow = subprocess.check_output('ls').decode().split('\n')
+xx = 0
 
-  for flaga in krotkie_domeny:
-    if len(flaga) == n_dlugosc_min: 
-      najkrotsza_domena.append(flaga)
-  
-  wynik = 'Najdłuższa domena, '+str(len(najdluzsza_domena[0]))+' znaków, to: '
-  wynik+= ', '.join(najdluzsza_domena)+'\n'
-  wynik+= 'Najkrótsza domena, '+str(len(najkrotsza_domena[0]))+' znaków, to: '
-  wynik+= ', '.join(najkrotsza_domena)
+licz_files = 0
+licz_save = 0
+licz_nosave = 0
+for plik in lista_plikow:
+  plik_ext = plik.split('.')[-1]
+  if type(plik) == str and plik_ext == "py":
+    licz_files+= 1
+    if check_code_safety(os.getcwd()+"/"+plik):
+      licz_save+= 1
+    else:
+      licz_nosave+= 1
+    xx+= 1
 
-  return wynik
+print('Przeszukanych plikow:',licz_files)
+print('Bezpiecznych plikow:',licz_save)
+print('Niebezpiecznych plikow:',licz_nosave)
 
-url = "https://zajecia-programowania-xd.pl/flagi"
-lista_flag = stworz_liste_flag(url)
-wynik = najdluzsza_najkrotsza_domena1(lista_flag)
-print(wynik)
+def getWebPage(link):
+  import requests
+  website_response = requests.get(link)
+  website_tekst = website_response.text
 
+  website_lista_prep = website_tekst.split('\n')
 
-"""
-print('ls -d */')
-main_dir_list = subprocess.check_output('ls -d */', shell=True).decode().split('\n')
-print(main_dir_list)
-print('* '*30)
-print('* '*30)
-for dir in main_dir_list:
-	
-	try:
-		print(subprocess.check_output('ls '+dir+'*.py', shell=True).decode().split('\n'))
-	except:
-		print('ls '+dir+'*.py')
+  return website_lista_prep
 
+def parseAdUrlFromAdsList(listing_page_url):
+  from bs4 import BeautifulSoup
+  #print(len(listing_page_url))
+  website_lista_prep = getWebPage(listing_page_url)
+  for tmp_str in website_lista_prep:
+    #print(type(tmp_str),tmp_str)
+    try:
+      soup = BeautifulSoup(tmp_str, "lxml")
+      href = soup.a['href']
+    except:
+      pass
 
-#dir_list = subprocess.check_output('ls -d rsync-home-directory/*', shell=True)
+  return href
 
-git_out = "" #subprocess.check_output('git clone https://github.com/v0jt4s13/python-test ./github-clone/', shell=True)
-
-rsync_executed_line = 'rsync -Pavp --rsh="ssh -i ~/.ssh/aws-python-xd.pem" '+const_var('USER')+'@'+const_var('DOMAIN')+':~/ ./rsync-home-directory/'
-#print('rsync -Pavp --rsh="ssh -i ~/.ssh/aws-python-xd.pem" ubuntu@pp.marzec.eu:~/python-test/ ./rsync-files/')
-
-#rsync_wynik = subprocess.check_output(rsync_executed_line, shell=True)
-#import_lines_list = subprocess.check_output('less github-clone/*.py |grep import', shell=True)
-
-#print(rsync_wynik)
-
-
-#print(git_out,rsync_wynik,import_lines_list)
-#Usage: rsync [OPTION]... SRC [SRC]... DEST
-#  or   rsync [OPTION]... SRC [SRC]... [USER@]HOST:DEST
-#  or   rsync [OPTION]... SRC [SRC]... [USER@]HOST::DEST
-#  or   rsync [OPTION]... SRC [SRC]... rsync://[USER@]HOST[:PORT]/DEST
-#  or   rsync [OPTION]... [USER@]HOST:SRC [DEST]
-#  or   rsync [OPTION]... [USER@]HOST::SRC [DEST]
-#  or   rsync [OPTION]... rsync://[USER@]HOST[:PORT]/SRC [DEST]
-#The ':' usages connect via remote shell, while '::' & 'rsync://' usages connect
-#to an rsync daemon, and require SRC or DEST to start with a module name.
-
-"""
+lista = "https://londynek.net/buysell/view-ads?cat=1&start=1"
+print(parseAdUrlFromAdsList(lista))
