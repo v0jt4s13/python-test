c5d2595c67e4b162c8a4ae90805d19871d304eb1
Wojtek <github@wmarzec.eu>
2022-03-12 01:51:12 +0100

    pass generator

--- flask_server/app_files/password_generator.py
+++ flask_server/app_files/password_generator.py
@@ -25,41 +25,43 @@ def listRandomSort(lista):
 
 def password_generator(char_count):
 
-	punctuation = "!#$%&*+,.:;=?@" #string.punctuation
-	haslo_znaki_lista = []
-	xx = 0
-	while xx < 2:
-		haslo_znaki_lista.append(str(random.sample(punctuation, 1)[0]))
-		haslo_znaki_lista.append(str(random.randrange(0,9)))
-		haslo_znaki_lista.append(str(random.choice(string.ascii_lowercase)))
-		haslo_znaki_lista.append(str(random.choice(string.ascii_uppercase)))
-		xx+= 1
-
-	xx = 8
-	while xx < char_count:
-		num = random.random()*1000
-		first = int(str(num)[0])
-		last = int(str(num)[-1])
-		rand_number = random.randrange(0,9)
-		if first <= 3:
-			haslo_znaki_lista.append(str(first))
+	if type(char_count) == int:
+		punctuation = "!#$%&*+,.:;=?@" #string.punctuation
+		haslo_znaki_lista = []
+		xx = 0
+		while xx < 2:
+			haslo_znaki_lista.append(str(random.sample(punctuation, 1)[0]))
+			haslo_znaki_lista.append(str(random.randrange(0,9)))
+			haslo_znaki_lista.append(str(random.choice(string.ascii_lowercase)))
+			haslo_znaki_lista.append(str(random.choice(string.ascii_uppercase)))
 			xx+= 1
-			continue
-		elif last <= 6:
-			if int(first+last) % 2 != 0:
-				char = random.choice(string.ascii_lowercase)
+
+		xx = 8
+		while xx < char_count:
+			num = random.random()*1000
+			first = int(str(num)[0])
+			last = int(str(num)[-1])
+			rand_number = random.randrange(0,9)
+			if first <= 3:
+				haslo_znaki_lista.append(str(first))
+				xx+= 1
+				continue
+			elif last <= 6:
+				if int(first+last) % 2 != 0:
+					char = random.choice(string.ascii_lowercase)
+				else:
+					char = random.choice(string.ascii_uppercase)
+				haslo_znaki_lista.append(char)
+				xx+= 1
+				continue
 			else:
-				char = random.choice(string.ascii_uppercase)
-			haslo_znaki_lista.append(char)
-			xx+= 1
-			continue
-		else:
-			char = str(random.sample(punctuation, 1)[0])
-			haslo_znaki_lista.append(char)
-			xx+= 1
-			continue
-
-
+				char = str(random.sample(punctuation, 1)[0])
+				haslo_znaki_lista.append(char)
+				xx+= 1
+				continue
+	else:
+		haslo_znaki_lista = "Cyfry miały być!"
+  
 	haslo = [''.join(haslo_znaki_lista)]
 	haslo.append(listRandomSort(haslo_znaki_lista))
 	return haslo[1]
