c0d22e7e95bd33f2a0b11f0aef12f567094d7fb5
Wojtek <github@wmarzec.eu>
2022-03-16 09:45:27 +0100

    random python code updates

--- flask_server/app_files/random_python_code.py
+++ flask_server/app_files/random_python_code.py
@@ -1,19 +1,108 @@
-from moje_biblioteki import convertListToJsonString
+try:
+	from moje_biblioteki import convertListToJsonString
+except:
+	def convertListToJsonString(input_list="",extra_para=""):
+
+		wrap_json_list = ""
+		prep_json_str = ""
+		prep_json_list = []
+		
+		if input_list == "":
+			input_list = ["Value1", "Value2", "Value3"]
+			append_new_list = []
+			for item in input_list:
+				more_details_list = ["More details1", "More details2", "More details3"] #extended_list_function(item)
+				if extra_para == "test":
+					append_new_list.append({'Len':len(more_details_list),'Detail1':more_details_list[0],'DetailLast':more_details_list[-1]})
+				else:
+					append_new_list.append({'Detail1':more_details_list[0],'Detail2':more_details_list[1],'DetailLast':more_details_list[-1]})
+			
+			wrap_json_list = {'MainName':append_new_list}
+
+			return wrap_json_list
+
+		else:
+			json_str_list = []
+			yy = 0
+			for item in input_list:
+				#if yy > 0: break
+				#print(yy,'==========')
+				#print(item)
+				#print(yy,'==========')
+		
+				if len(item) > 0:
+					xx = 0
+					
+					while xx < len(item):
+						json_parent = ""
+						json_child = ""
+						prep_json_str = ""
+						#if xx > 0: prep_json_str+= ', '
+						if len(item[xx]) == 1:
+							xx+= 1
+							continue
+						if type(item[xx][1]) == list:
+							#print('pass',len(item[xx][1]),item[xx][1])
+							zz = 0
+							prep_json_str = ""
+							tmp_str_pun_list = []
+							while zz < len(item[xx][1]):
+								key = item[xx][1][zz][0]
+								val = item[xx][1][zz][1]
+
+								prep_json_str = {
+									key:val
+								}
+								tmp_str_pun_list.append(prep_json_str)
+								zz+= 1
+							json_child = prep_json_str
+							#print('11==>',tmp_str_pun_list)
+							prep_json_list.append(tmp_str_pun_list)
+				
+						else:
+							key = item[xx][0]
+							val = item[xx][1]
+
+							prep_json_str = {
+								key:val
+							}
+							prep_json_list.append(prep_json_str)
+
+							#print('22==>',prep_json_str)
+
+						#print('33==>',prep_json_list)
+						xx+= 1
+					prep_json_list_str = ', '.join(map(str,prep_json_list))
+					#print('44==>',prep_json_list_str)
+					#json_obj = json.dumps(prep_json_list_str, indent = 4)
+					#print('55==>',json.loads(json_obj))
+					json_str_list.append(prep_json_list)
+				else:
+					break
+
+				yy+= 1
+
+				return json_str_list
 
 def getRandomPythonExampleCode():
 	import random
 	
-	rand_num = random.randrange(len(example_item_list))
 	example_item_list = pythonCodeExampleList()
+	rand_num = random.randrange(len(example_item_list))
 
 	return example_item_list[rand_num]
 
 def showRandomPythonCode(output_type="html"):
 	
-	licz = 0
-	start = True
-	while start:
-		if output_type == "console":
+	if output_type == "html":
+
+		code_list = getRandomPythonExampleCode()
+		print(code_list)
+  
+	else:
+		licz = 0
+		start = True
+		while start:
 			if licz > 0:
 				if input("Wylosować kolejny przykład (T/N) ? ") not in ("T","t", ""):
 					break
@@ -43,12 +132,15 @@ def showRandomPythonCode(output_type="html"):
 			use_examples_list = code_list[4][1]
 		except:
 			use_examples_list = []
+		try:
+			example_return_list = code_list[5]
+		except:
+			example_return_list = []
 
-
-		renderRandomPythonCodeConsoleOutput(method_list, name_list, syntax_list, parameters_list, use_examples_list)
+		renderRandomPythonCodeConsoleOutput(method_list, name_list, syntax_list, parameters_list, use_examples_list, example_return_list)
 		
 
-def renderRandomPythonCodeConsoleOutput(method_list, name_list, syntax_list, parameters_list, use_examples_list):
+def renderRandomPythonCodeConsoleOutput(method_list, name_list, syntax_list, parameters_list, use_examples_list, example_return_list):
 
 		if method_list[0] not in ("Others"):
 			print('\n\n\n\t'+'*'*6,method_list[0]+':',name_list[0]+'() ****\n')
@@ -79,9 +171,9 @@ def renderRandomPythonCodeConsoleOutput(method_list, name_list, syntax_list, par
 			print('\t'+'_ '*30)
 			print('\n\t| ',use_examples_list[xx][1].replace('\n','\n\t|  '))
 			print('\t'+'_ '*30)
-			if len(code_list[5]) > 1:
+			if len(example_return_list) > 1:
 				try:
-					example_return_info_list = code_list[5][1]
+					example_return_info_list = example_return_list[1]
 				except:
 					example_return_info_list = []
 				print()
@@ -92,9 +184,9 @@ def renderRandomPythonCodeConsoleOutput(method_list, name_list, syntax_list, par
 	
 		print('\n\n')
 		
-def renderRandomPythonCodeWebsiteOutput(method_list, name_list, syntax_list, parameters_list, use_examples_list):
+def renderRandomPythonCodeWebsiteOutput(method_list, name_list, syntax_list, parameters_list, use_examples_list, example_return_list):
 
-	return method_list, name_list, syntax_list, parameters_list, use_examples_list
+	return method_list, name_list, syntax_list, parameters_list, use_examples_list, example_return_list
 
 
 """
@@ -138,9 +230,9 @@ def chwilowa():
 		print('\t'+'_ '*30)
 		output_str = '\t| Example description:',use_examples_list[xx][0]
 		output_str = '\n\t| ',use_examples_list[xx][1].replace('\n','\n\t|  '
-		if len(code_list[5]) > 1:
+		if len(example_return_list) > 1:
 			try:
-				example_return_info_list = code_list[5][1]
+				example_return_info_list = example_return_list[1]
 			except:
 				example_return_info_list = []
 			print()
--- test-randompythoncode.py
+++ test-randompythoncode.py
@@ -1,8 +1,12 @@
 try:
-  from flask_server.app_files.random_python_code import getRandomPythonExampleCode
+	from flask_server.app_files.random_python_code import getRandomPythonExampleCode, showRandomPythonCode
 except:
-  from app_files.random_python_code import getRandomPythonExampleCode
-  
+	from app_files.random_python_code import getRandomPythonExampleCode, showRandomPythonCode
+
+from rich.console import Console
+# creating the console object
+console = Console(width=120)
+	
 # [['Type', 'Metoda'], ['Name', 'Sort'], ['Syntax', 'list.sort(reverse=True|False, key=myFunc)'], 
 #   ['ParameterValue', [
 #     ['reverse', 'Optional. reverse=True will sort the list descending. Default is reverse=False'], 
@@ -19,90 +23,95 @@ except:
 
 #showRandomPythonCode()
 
-if 1 == 1:
-  
-  getRandomPythonExampleCode()
+if input("HTML czy konsola (H/C) ? ") in ("H","h", ""):
+	
+	showRandomPythonCode()
 
 else:
+	
+	licz = 0
+	start = True
+	while start:
+		if licz > 0:
+			if input("Wylosować kolejny przykład (T/N) ? ") not in ("T","t", ""):
+				break
+		console.clear()
   
-  licz = 0
-  start = True
-  while start:
-    if licz > 0:
-      if input("Wylosować kolejny przykład (T/N) ? ") not in ("T","t", ""):
-        break
+		licz+= 1
+		show_code_list = getRandomPythonExampleCode()
+		#print(show_code_list[0][0])
+		print('\t\t\t ******** Losowanie nr: %i *******' %licz)
 
-    licz+= 1
-    show_code_list = getRandomPythonExampleCode()
-    #print(show_code_list[0][0])
-    print('\t\t\t ******** Losowanie nr: %i *******' %licz)
+		try:
+			method_list = [show_code_list[0][1],show_code_list[0][0]]
+		except:
+			method_list = []
+		try:
+			name_list = [show_code_list[1][1],show_code_list[1][0]]
+		except:
+			name_list = []
+		try:  
+			syntax_list = [show_code_list[2][1],show_code_list[2][0]]
+		except:
+			syntax_list = []
+		try:
+			parameters_list = show_code_list[3][1]
+		except:
+			parameters_list = []
+		try:
+			use_examples_list = show_code_list[4][1]
+		except:
+			use_examples_list = []
+		try:
+			example_return_list = code_list[5]
+		except:
+			example_return_list = []
 
-    try:
-      method_list = [show_code_list[0][1],show_code_list[0][0]]
-    except:
-      method_list = []
-    try:
-      name_list = [show_code_list[1][1],show_code_list[1][0]]
-    except:
-      name_list = []
-    try:  
-      syntax_list = [show_code_list[2][1],show_code_list[2][0]]
-    except:
-      syntax_list = []
-    try:
-      parameters_list = show_code_list[3][1]
-    except:
-      parameters_list = []
-    try:
-      use_examples_list = show_code_list[4][1]
-    except:
-      use_examples_list = []
+		#print(name_list)
+		#print(method_list)
+		#print(syntax_list)
+		if method_list[0] not in ("Others"):
+			print('\n\n\n\t'+'*'*6,method_list[0]+':',name_list[0]+'() ****\n')
 
-    #print(name_list)
-    #print(method_list)
-    #print(syntax_list)
-    if method_list[0] not in ("Others"):
-      print('\n\n\n\t'+'*'*6,method_list[0]+':',name_list[0]+'() ****\n')
+		else:
+			print('\n\n\n\t'+'*'*6,name_list[0]+' ****\n')
+		
+		if len(syntax_list) > 0:
+			print('\t'+syntax_list[1]+': '+syntax_list[0])
 
-    else:
-      print('\n\n\n\t'+'*'*6,name_list[0]+' ****\n')
-    
-    if len(syntax_list) > 0:
-      print('\t'+syntax_list[1]+': '+syntax_list[0])
+		if len(parameters_list) > 0:
+			print('')
+			#print('\tParameters type is %s and length: %i ' %(type(parameters_list),len(parameters_list)))
+			print('\t'+'_ '*30,'\n')
+			print('\tParameter\tDescription')
+			print('\t'+'_ '*30,'\n')
+			xx = 0
+			for val in parameters_list:
+				if xx > 0: print()
+				print('\t'+val[0]+'\t\t'+val[1])
+				xx+= 1
 
-    if len(parameters_list) > 0:
-      print('')
-      #print('\tParameters type is %s and length: %i ' %(type(parameters_list),len(parameters_list)))
-      print('\t'+'_ '*30,'\n')
-      print('\tParameter\tDescription')
-      print('\t'+'_ '*30,'\n')
-      xx = 0
-      for val in parameters_list:
-        if xx > 0: print()
-        print('\t'+val[0]+'\t\t'+val[1])
-        xx+= 1
+		#print('')
+		xx = 0
+		while xx < len(use_examples_list):
+			if xx == 0: print('\t'+'_ '*30,'\n')
+			elif xx > 0: print('')
+			print('\t| Example description:',use_examples_list[xx][0])
+			print('\t'+'_ '*30)
+			print('\n\t| ',use_examples_list[xx][1].replace('\n','\n\t|  '))
+			print('\t'+'_ '*30)
 
-    #print('')
-    xx = 0
-    while xx < len(use_examples_list):
-      if xx == 0: print('\t'+'_ '*30,'\n')
-      elif xx > 0: print('')
-      print('\t| Example description:',use_examples_list[xx][0])
-      print('\t'+'_ '*30)
-      print('\n\t| ',use_examples_list[xx][1].replace('\n','\n\t|  '))
-      print('\t'+'_ '*30)
-
-      if len(show_code_list[5]) > 1:
-        try:
-          example_return_info_list = show_code_list[5][1]
-        except:
-          example_return_info_list = []
-        print()
-        print('\t'+example_return_info_list[0][0]+':',example_return_info_list[0][1])
-        print('\t'+example_return_info_list[1][0]+':',example_return_info_list[1][1])
-        example_return_info_list.clear()
-      xx+= 1
-  
-    print('\n\n')
-    
+			if len(example_return_list) > 1:
+				try:
+					example_return_info_list = example_return_list[1]
+				except:
+					example_return_info_list = []
+				print()
+				print('\t'+example_return_info_list[0][0]+':',example_return_info_list[0][1])
+				print('\t'+example_return_info_list[1][0]+':',example_return_info_list[1][1])
+				example_return_info_list.clear()
+			xx+= 1
+	
+		print('\n\n')
+		
 
